592aa64940c3b6f6ecc1f82188b04d8435111a2a
Kayky <kayky.paulinoalves@gmail.com>
2023-05-28 21:56:47 -0300

    Projeto Finalizado

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('read_button');
+
+button.addEventListener('click', function(){
+    const card = document.querySelector('.card');
+    card.classList.toggle('active');
+    if(card.classList.contains('active')){
+        return button.textContent = 'Read less'
+    }
+    
+    button.textContent = 'Read more'
+})
